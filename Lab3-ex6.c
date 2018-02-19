7580fefa8f9afbadcfaf1f98b8298f58c7f35986
John <scjc68@tc-m610-login-node623.cobalt.lan>
2018-02-19 03:28:38 -0600

    Example 6 Fix

--- Lab3-ex6.c
+++ Lab3-ex6.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(void) 
 {
-	char *ch;
-	*ch=malloc(sizeof(char)*2);    
+	char ch[3];
+	ch[3]=malloc(sizeof(char)*2);    
 	ch[1]='3';
 	ch[2]='a';
 	while (ch[1] > '0') 
