7bc9a7993df069417778e5c6e3e7dde4675e7c5d
John <scjc68@tc-m610-login-node623.cobalt.lan>
2018-02-19 02:21:09 -0600

    Example 1 Fix

--- Lab3-ex1.c
+++ Lab3-ex1.c
@@ -4,15 +4,21 @@ int main()
 {
     char* fileName;
     printf("Specify file name you would like to print to: \n");
-    scanf("%s",&fileName);
+    scanf("%s", fileName);
  
-
-    FILE* file1 = fopen(fileName, "a+");
+FILE* file1;
+    file1 = fopen(fileName, "ab+");
+	
+	if (!file1) {
+		printf("unable to open file");
+		file1 = fopen(fileName, "wb");
+		//return 0;
+	}
  
     char c;
     while ((c=getchar()) != EOF)
     {
-        fprintf(file1, &c);
+        fprintf(file1, "%c" ,c);
     }
  
     fclose(file1);
