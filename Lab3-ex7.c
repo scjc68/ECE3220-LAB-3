ea2770fc895ed5e8a8b8c319e69647f3fbb65671
John <scjc68@tc-m610-login-node623.cobalt.lan>
2018-02-19 03:34:11 -0600

    Example 7 Fix

--- Lab3-ex7.c
+++ Lab3-ex7.c
@@ -4,6 +4,6 @@ int main()
 	int x=1309;
 	int *p = NULL;
 	printf("\n The value of x is = %d",x);
-	printf("\n The pointer points to the value = %d",*p);
+	printf("\n The pointer points to the value = %d",&x);
 	return 1;
 }
